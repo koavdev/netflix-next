01c902ac39db8ceaa07fc23c147e5b9c87cc146d
koavdev <koavdev@gmail.com>
2023-07-21 02:17:30 -0300

    feat: add InfoModal component

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { BsFillPlayFill } from 'react-icons/bs';
+import { BiChevronDown } from 'react-icons/bi';
 import FavoriteButton from './FavoriteButton';
+import useInfoModal from '@/hooks/useInfoModal';
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -11,6 +13,8 @@ const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
     const router = useRouter();
+    const { openModal } = useInfoModal();
+
     return (
         <div className='group bg-zinc-900 col-span relative h-[12vw]'>
             <img
@@ -92,6 +96,10 @@ const MovieCard: React.FC<MovieCardProps> = ({
                             <BsFillPlayFill size={30} />
                         </div>
                         <FavoriteButton movieId={data?.id} />
+                        <div className='cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300'
+                            onClick={() => openModal(data?.id)}>
+                            <BiChevronDown className='text-white group-hover/item:text-neutral-300' size={30}/>
+                        </div>
                     </div>
 
                     <p className='text-green-400 font-semibold mt-4'>
